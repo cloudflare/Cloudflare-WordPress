df0e0cefde231a79888edd3a0f836ff9fe5871c8
Furkan Yilmaz <mufuyil@gmail.com>
2016-05-31 14:40:46 -0700

    created WordPressAPI.php

---
+++ src/Wordpress/WordPressAPI.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CF\WordPress;
+
+class WordPressAPI implements IntegrationAPIInterface 
+{
+    /**
+     * @param $domain_name
+     * @return mixed
+     */
+    public function getDNSRecords($domain_name) { return null }
+
+    /**
+     * @param $domain_name
+     * @param DNSRecord $DNSRecord
+     * @return mixed
+     */
+    public function addDNSRecord($domain_name, DNSRecord $DNSRecord) { return null }
+
+    /**
+     * @param $domain_name
+     * @param DNSRecord $DNSRecord
+     * @return mixed
+     */
+    public function editDNSRecord($domain_name, DNSRecord $DNSRecord) { return null }
+
+    /**
+     * @param $domain_name
+     * @param DNSRecord $DNSRecord
+     * @return mixed
+     */
+    public function removeDNSRecord($domain_name, DNSRecord $DNSRecord) { return null }
+
+    /**
+     * @return mixed
+     */
+    public function getHostAPIKey() { return null }
+
+    /**
+     * @param null $userId
+     * @return mixed
+     */
+    public function getDomainList($userId = null) { return null }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId() 
+    {
+        return $dataStore->getCloudFlareEmail();
+    }
+}
+
+
+}
