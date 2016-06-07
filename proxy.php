b62703d1e21576736d89b511a8ccd41aa09ede9a
Furkan Yilmaz <mufuyil@gmail.com>
2016-06-06 17:15:48 -0700

    added require wp-load.php

--- proxy.php
+++ proxy.php
@@ -1,28 +1,27 @@
 <?php
-require_once("vendor/autoload.php");
 
-// TODO: the following code gives error. Require wp-load.php without error
-// include wp-load.php relatively
-// $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
-// require_once( $parse_uri[0] . 'wp-load.php' );
+require_once 'vendor/autoload.php';
 
+// include wp-load.php, directs logs to debug.log
+$parse_uri = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
+require_once $parse_uri[0].'wp-load.php';
 
 header('Content-Type: application/json');
 
-$config = new CF\Integration\DefaultConfig(file_get_contents("config.js"));
-$logger = new CF\Integration\DefaultLogger($config->getValue("debug"));
+$config = new CF\Integration\DefaultConfig(file_get_contents('config.js'));
+$logger = new CF\Integration\DefaultLogger($config->getValue('debug'));
 $dataStore = new CF\WordPress\DataStore($logger);
 $wordpressAPI = new CF\WordPress\WordPressAPI($dataStore);
 $wordpressIntegration = new CF\Integration\DefaultIntegration($config, $wordpressAPI, $dataStore, $logger);
 $clientAPIClient = new CF\API\Client($wordpressIntegration);
 $clientAPIClientRoutes = CF\WordPress\ClientRoutes::$routes;
-$pluginAPIClient = new CF\API\Client($wordpressIntegration);
+$pluginAPIClient = new CF\API\Plugin($wordpressIntegration);
 $pluginAPIPluginRoutes = CF\WordPress\PluginRoutes::$routes;
 
 $method = $_SERVER['REQUEST_METHOD'];
 $parameters = $_GET;
-$body = json_decode(file_get_contents('php://input'),true);
-$path = (strtoupper($method === "GET") ? $_GET['proxyURL'] : $body['proxyURL']);
+$body = json_decode(file_get_contents('php://input'), true);
+$path = (strtoupper($method === 'GET') ? $_GET['proxyURL'] : $body['proxyURL']);
 
 unset($parameters['proxyURL']);
 unset($body['proxyURL']);
@@ -32,21 +31,21 @@ $request = new CF\API\Request($method, $path, $parameters, $body);
 // TODO: change $wordpressAPI->getHostAPIKey() to something appropriate
 // since it's null
 $isCSRFTokenValid = false;
-$isCSRFTokenValid = ($request->getMethod() === "GET") ? true : CF\SecurityUtil::csrfTokenValidate($wordpressAPI->getHostAPIKey(), $wordpressAPI->getUserId(), $request->getBody()['cfCSRFToken']);
+$isCSRFTokenValid = ($request->getMethod() === 'GET') ? true : CF\SecurityUtil::csrfTokenValidate($wordpressAPI->getHostAPIKey(), $wordpressAPI->getUserId(), $request->getBody()['cfCSRFToken']);
 unset($body['cfCSRFToken']);
-$apiResponse = "";
+$apiResponse = '';
 $apiRouter = null;
 
-if(isClientAPI($request->getUrl())) {
-	$apiRouter = new CF\Router\DefaultRestAPIRouter($wordpressIntegration, $clientAPIClient, $clientAPIClientRoutes);
+if (isClientAPI($request->getUrl())) {
+    $apiRouter = new CF\Router\DefaultRestAPIRouter($wordpressIntegration, $clientAPIClient, $clientAPIClientRoutes);
 } else {
-	$apiRouter = new CF\Router\DefaultRestAPIRouter($wordpressIntegration, $pluginAPIClient, $pluginAPIPluginRoutes);
+    $apiRouter = new CF\Router\DefaultRestAPIRouter($wordpressIntegration, $pluginAPIClient, $pluginAPIPluginRoutes);
 }
 
-if($isCSRFTokenValid) {
+if ($isCSRFTokenValid) {
     $apiResponse = $apiRouter->route($request);
 } else {
-    $apiResponse = $apiRouter->getAPIClient()->createAPIError("CSRF Token not valid.");
+    $apiResponse = $apiRouter->getAPIClient()->createAPIError('CSRF Token not valid.');
     // TODO: We should add return error response here.
 }
 
@@ -54,8 +53,10 @@ echo json_encode($apiResponse);
 
 /**
  * @param $path
+ *
  * @return bool
  */
-function isClientAPI($path) {
-    return (strpos($path, CF\API\Client::ENDPOINT) !== false);
+function isClientAPI($path)
+{
+    return strpos($path, CF\API\Client::ENDPOINT) !== false;
 }
