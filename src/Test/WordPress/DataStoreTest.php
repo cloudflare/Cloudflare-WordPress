46e58498aa7fbc714bc4a67e5b213c2d948c64a8
Furkan Yilmaz <mufuyil@gmail.com>
2016-08-18 15:00:04 +0100

    PI-736 added more tests for DataStore

--- src/Test/WordPress/DataStoreTest.php
+++ src/Test/WordPress/DataStoreTest.php
@@ -1,40 +1,100 @@
 <?php
 
-namespace CF\Test\WordPress;
-
-use CF\WordPress\DataStore;
-
-class DataStoreTest extends \PHPUnit_Framework_TestCase
-{
-    public static $keyValueStore = array();
-    protected $dataStore;
-    protected $mockLogger;
-
-    public function setup()
+namespace CF\Wordpress {
+    function update_option($key, $value)
     {
-        $this->mockLogger = $this->getMockBuilder('CF\Integration\DefaultLogger')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->dataStore = new DataStore($this->mockLogger);
+        return true;
     }
 
-    public function testGetHostAPIUserUniqueIdReturnsNull()
+    function get_option($key)
     {
-        $this->assertNull($this->dataStore->getHostAPIUserUniqueId());
+        return $key;
     }
+}
 
-    public function testGetHostAPIUserKeyReturnsNull()
-    {
-        $this->assertNull($this->dataStore->getHostAPIUserKey());
-    }
+namespace CF\Test\WordPress {
 
-    public function testGetPluginSettingReturnsFalseForBadSetting()
-    {
-        $this->assertFalse($this->dataStore->getPluginSetting('nonExistentSetting'));
-    }
+    use CF\WordPress\DataStore;
+    use CF\API\Plugin;
 
-    public function testSetPluginSettingReturnsFalseForBadSettingName()
+    class DataStoreTest extends \PHPUnit_Framework_TestCase
     {
-        $this->assertFalse($this->dataStore->setPluginSetting('nonExistentSetting', 'value'));
+        public static $keyValueStore = array();
+        protected $dataStore;
+        protected $mockLogger;
+
+        public function setup()
+        {
+            $this->mockLogger = $this->getMockBuilder('CF\Integration\DefaultLogger')
+                ->disableOriginalConstructor()
+                ->getMock();
+            $this->dataStore = new DataStore($this->mockLogger);
+        }
+
+        public function testGetHostAPIUserUniqueIdReturnsNull()
+        {
+            $this->assertNull($this->dataStore->getHostAPIUserUniqueId());
+        }
+
+        public function testGetClientV4APIKeyReturnsAPIKey()
+        {
+            $this->assertEquals(DataStore::API_KEY, $this->dataStore->getClientV4APIKey());
+        }
+
+        public function testGetHostAPIUserKeyReturnsNull()
+        {
+            $this->assertNull($this->dataStore->getHostAPIUserKey());
+        }
+
+        public function testGetCloudFlareEmailReturnsEmail()
+        {
+            $this->assertEquals(DataStore::EMAIL, $this->dataStore->getCloudFlareEmail());
+        }
+
+        public function testGetPluginSettingReturnsSettingName()
+        {
+            $setting = Plugin::SETTING_IP_REWRITE;
+            $result = $this->dataStore->getPluginSetting($setting);
+
+            $this->assertEquals($setting, $result);
+        }
+
+        public function testGetPluginSettingReturnsFalseForBadSetting()
+        {
+            $setting = 'Bad Setting';
+            $result = $this->dataStore->getPluginSetting($setting);
+
+            $this->assertFalse($result);
+        }
+
+        public function testSetPluginSettingReturnsTrueForCorrectSettingName()
+        {
+            $setting = Plugin::SETTING_IP_REWRITE;
+            $value = 'justAValue';
+            $result = $this->dataStore->setPluginSetting($setting, $value);
+
+            $this->assertTrue($result);
+        }
+
+        public function testSetPluginSettingReturnsFalseForBadSettingName()
+        {
+            $setting = 'justASetting';
+            $value = 'justAValue';
+            $result = $this->dataStore->setPluginSetting($setting, $value);
+
+            $this->assertFalse($result);
+        }
+
+        public function testGetCallsGetOption()
+        {
+            $option = 'option';
+            $response = $this->dataStore->get($option);
+            $this->assertEquals($option, $response);
+        }
+
+        public function testSetCallsUpdateOption()
+        {
+            $this->assertTrue($this->dataStore->set('key', 'value'));
+        }
     }
 }
