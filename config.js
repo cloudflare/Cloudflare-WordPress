54c6f04bd2535da4cd9157ace5360bf35cf88083
Furkan Yilmaz <mufuyil@gmail.com>
2016-09-21 18:14:27 +0100

    Version bump to 3.0.3

--- config.js
+++ config.js
@@ -1,31 +1,42 @@
 {
-    "debug": false,
-    "featureManagerIsAlwaysOnlineEnabled": true,
-    "featureManagerIsBrowserCacheTTLEnabled": true,
-    "featureManagerIsBrowserIntegrityCheckEnabled": true,
-    "featureManagerIsCacheLevelEnabled": true,
-    "featureManagerIsChallengePassageEnabled": true,
-    "featureManagerIsDevelopmentModeEnabled": true,
-    "featureManagerIsFullZoneProvisioningEnabled": false,
-    "featureManagerIsImageOptimizationEnabled": true,
-    "featureManagerIsIpv6Enabled": true,
-    "featureManagerIsIpRewriteEnabled": true,
-    "featureManagerIsMinifyEnabled": true,
-    "featureManagerIsProtocolRewriteEnabled": true,
-    "featureManagerIsPurgeCacheEnabled": true,
-    "featureManagerIsRailgunEnabled": true,
-    "featureManagerIsScanEnabled": false,
-    "featureManagerIsSecurityLevelEnabled": true,
-    "featureManagerIsSSLEnabled": true,
-    "featureManagerIsWAFEnabled": true,
-    "isSubdomainCheckEnabled": true,
-    "homePageCards": ["ApplyDefaultSettingsCard", "PurgeCacheCard", "PluginSpecificCacheCard"],
+    "debug": false, 
+    "featureManagerIsAlwaysOnlineEnabled": true, 
+    "featureManagerIsBrowserCacheTTLEnabled": true, 
+    "featureManagerIsBrowserIntegrityCheckEnabled": true, 
+    "featureManagerIsCacheLevelEnabled": true, 
+    "featureManagerIsChallengePassageEnabled": true, 
+    "featureManagerIsDevelopmentModeEnabled": true, 
+    "featureManagerIsFullZoneProvisioningEnabled": false, 
+    "featureManagerIsImageOptimizationEnabled": true, 
+    "featureManagerIsIpRewriteEnabled": true, 
+    "featureManagerIsIpv6Enabled": true, 
+    "featureManagerIsMinifyEnabled": true, 
+    "featureManagerIsProtocolRewriteEnabled": true, 
+    "featureManagerIsPurgeCacheEnabled": true, 
+    "featureManagerIsRailgunEnabled": true, 
+    "featureManagerIsSSLEnabled": true, 
+    "featureManagerIsScanEnabled": false, 
+    "featureManagerIsSecurityLevelEnabled": true, 
+    "featureManagerIsWAFEnabled": true, 
+    "homePageCards": [
+        "ApplyDefaultSettingsCard", 
+        "PurgeCacheCard", 
+        "PluginSpecificCacheCard"
+    ], 
+    "integrationName": "wordpress", 
+    "isSubdomainCheckEnabled": true, 
+    "locale": "en", 
     "moreSettingsCards": {
-        "container.moresettings.speed": ["AlwaysOnlineCard", "ImageOptimizationCard"],
-        "container.moresettings.security": ["SecurityLevelCard", "WAFCard", "AdvanceDDoSCard"]
-    },
-    "locale": "en",
-    "integrationName": "wordpress",
-    "useHostAPILogin": false,
-    "version": "3.0.2"
+        "container.moresettings.security": [
+            "SecurityLevelCard", 
+            "WAFCard", 
+            "AdvanceDDoSCard"
+        ], 
+        "container.moresettings.speed": [
+            "AlwaysOnlineCard", 
+            "ImageOptimizationCard"
+        ]
+    }, 
+    "useHostAPILogin": false, 
+    "version": "3.0.3"
 }
