85e50c203003ac53b34d6dc05ac6cbf23f4d7bdc
XhmikosR <xhmikosr@gmail.com>
2016-11-24 16:58:09 +0200

    Lint.

--- src/Test/WordPress/PlansTest.php
+++ src/Test/WordPress/PlansTest.php
@@ -9,24 +9,24 @@ class PlansTest extends \PHPUnit_Framework_TestCase
     public function testPlanNeedsUpgradeAllCases()
     {
         // FREE_PLAN
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::FREE_PLAN, Plans::FREE_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::FREE_PLAN, Plans::PRO_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::FREE_PLAN, Plans::BIZ_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::FREE_PLAN, Plans::ENT_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::FREE_PLAN, Plans::FREE_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::FREE_PLAN, Plans::PRO_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::FREE_PLAN, Plans::BIZ_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::FREE_PLAN, Plans::ENT_PLAN));
 
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::PRO_PLAN, Plans::FREE_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::PRO_PLAN, Plans::PRO_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::PRO_PLAN, Plans::BIZ_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::PRO_PLAN, Plans::ENT_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::PRO_PLAN, Plans::FREE_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::PRO_PLAN, Plans::PRO_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::PRO_PLAN, Plans::BIZ_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::PRO_PLAN, Plans::ENT_PLAN));
 
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::BIZ_PLAN, Plans::FREE_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::BIZ_PLAN, Plans::PRO_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::BIZ_PLAN, Plans::BIZ_PLAN));
-        $this->assertTrue(Plans::PlanNeedsUpgrade(Plans::BIZ_PLAN, Plans::ENT_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::BIZ_PLAN, Plans::FREE_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::BIZ_PLAN, Plans::PRO_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::BIZ_PLAN, Plans::BIZ_PLAN));
+        $this->assertTrue(Plans::planNeedsUpgrade(Plans::BIZ_PLAN, Plans::ENT_PLAN));
 
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::ENT_PLAN, Plans::FREE_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::ENT_PLAN, Plans::PRO_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::ENT_PLAN, Plans::BIZ_PLAN));
-        $this->assertFalse(Plans::PlanNeedsUpgrade(Plans::ENT_PLAN, Plans::ENT_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::ENT_PLAN, Plans::FREE_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::ENT_PLAN, Plans::PRO_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::ENT_PLAN, Plans::BIZ_PLAN));
+        $this->assertFalse(Plans::planNeedsUpgrade(Plans::ENT_PLAN, Plans::ENT_PLAN));
     }
 }
