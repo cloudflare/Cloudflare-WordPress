b29f2f2442515a8c9cfb396fe08d435d96d21209
Furkan Yilmaz <mufuyil@gmail.com>
2017-02-03 14:38:46 +0000

    Update frontend to v2.6.3

---
+++ scripts/sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## DESCRIPTION
+# This is a script to easily test WordPress Plugin Code on your own server
+# Example usage: ./script/sync.sh root domain.com
+#$1 = username
+#$2 = hostname
+
+## TROUBLESHOOT
+# If you have issues using hostname it's probably because the hostname 
+# has cloudflare IP. In that case you'll need to use IP address instead of
+# hostname
+
+PLUGINPATH="/var/www/html/wordpress/wp-content/plugins/cloudflare"
+USRHOSTFLDR="$1@$2:$PLUGINPATH"
+
+rsync -azuvP --delete ./ $USRHOSTFLDR
