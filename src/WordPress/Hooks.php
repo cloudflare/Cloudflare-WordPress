ae824877c9465aaceadb54a1349d5ee2387a435f
Alex <self@pirsig.net>
2018-03-06 13:47:05 +0100

    Update Hooks.php

--- src/WordPress/Hooks.php
+++ src/WordPress/Hooks.php
@@ -130,7 +130,7 @@ class Hooks
         if ($this->isPluginSpecificCacheEnabled()) {
             $wpDomainList = $this->integrationAPI->getDomainList();
             $wpDomain = $wpDomainList[0];
-            if (count($wpDomain) <= 0) {
+            if (!isset($wpDomain) || count($wpDomain) <= 0) {
                 return;
             }
 
