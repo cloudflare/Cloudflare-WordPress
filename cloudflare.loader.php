e87ff7c8f447c3fff5d787f55bbd3eae2627c9a8
Alex <self@pirsig.net>
2020-06-26 15:37:10 +0200

    Update cloudflare.loader.php (#212)
    * Update cloudflare.loader.php
    
    Allow custom actions for purge url and purge everything actions
    
    Co-authored-by: Patryk Szczygłowski <patryk@patryk.net>

--- cloudflare.loader.php
+++ cloudflare.loader.php
@@ -58,12 +58,14 @@ $cloudflarePurgeEverythingActions = array(
     'customize_save_after'              // Edit theme
 );
 
+$cloudflarePurgeEverythingActions = apply_filters('cloudflare_purge_everything_actions', $cloudflarePurgeEverythingActions);
+
 foreach ($cloudflarePurgeEverythingActions as $action) {
     add_action($action, array($cloudflareHooks, 'purgeCacheEverything'), PHP_INT_MAX);
 }
 
 /**
- * You can filter the list of URLs that get purged by Cloudflare after a post is 
+ * You can filter the list of URLs that get purged by Cloudflare after a post is
  * modified by implementing a filter for the "cloudflare_purge_by_url" hook.
  *
  * @Example:
@@ -79,12 +81,15 @@ foreach ($cloudflarePurgeEverythingActions as $action) {
  *
  * add_filter('cloudflare_purge_by_url', your_cloudflare_url_filter, 10, 2);
  */
+
 $cloudflarePurgeURLActions = array(
     'deleted_post',                     // Delete a post
     'edit_post',                        // Edit a post - includes leaving comments
     'delete_attachment',                // Delete an attachment - includes re-uploading
     'post_updated',                     // Update a post
 );
+
+$cloudflarePurgeURLActions = apply_filters('cloudflare_purge_url_actions', $cloudflarePurgeURLActions);
 
 foreach ($cloudflarePurgeURLActions as $action) {
     add_action($action, array($cloudflareHooks, 'purgeCacheByRevelantURLs'), PHP_INT_MAX, 2);
