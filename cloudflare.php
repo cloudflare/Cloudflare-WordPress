d44899ec843f964c11deb50157c4e8f1fb1cad01
Patryk Szczygłowski <patryk@patryk.net>
2020-06-26 13:33:35 +0100

    Clean up the code, add some documentation

--- cloudflare.php
+++ cloudflare.php
@@ -8,8 +8,17 @@ Author: Cloudflare, Inc.
 License: BSD-3-Clause
 */
 
-// To enable HTTP2 Server Push feature, add the following line to wp-config.php file
+// The following constants are available. Add them to wp-config.php to enable.
+
+// To enable HTTP/2 Server Push feature:
 // define('CLOUDFLARE_HTTP2_SERVER_PUSH_ACTIVE', true);
+
+// Cloudflare has a limit of how many resourses can be pushed by HTTP/2 Server Push
+// (3 KiB by default). Add the following to change that amount:
+// define('CLOUDFLARE_HTTP2_SERVER_PUSH_HEADER_SIZE', 3072);
+
+// To enable error logging when HTTP/2 Server Push header size is exceeded:
+// define('CLOUDFLARE_HTTP2_SERVER_PUSH_LOG', true);
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
