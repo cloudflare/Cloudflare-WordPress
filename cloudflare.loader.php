d7784bf47f79dcf3e4f758df5e7f75c696cfc203
inwerpsel <pieter.vincent1988@gmail.com>
2021-02-09 11:41:15 +0100

    Purge posts that go from public to private.
    * Add ...$args to the hook to avoid doing additional queries. This could
    likely be used elsewhere in the function instead of querying those
    things.
    * Make "private" also a "valid" post status, add extra check to still
    skip purge unless it's a status change.

--- cloudflare.loader.php
+++ cloudflare.loader.php
@@ -103,5 +103,5 @@ $cloudflarePurgeURLActions = array(
 $cloudflarePurgeURLActions = apply_filters('cloudflare_purge_url_actions', $cloudflarePurgeURLActions);
 
 foreach ($cloudflarePurgeURLActions as $action) {
-    add_action($action, array($cloudflareHooks, 'purgeCacheByRelevantURLs'), PHP_INT_MAX, 2);
+    add_action($action, array($cloudflareHooks, 'purgeCacheByRelevantURLs'), PHP_INT_MAX, 4);
 }
