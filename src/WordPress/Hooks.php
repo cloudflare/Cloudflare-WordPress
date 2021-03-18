e34d5c857125a1617cd16c57d914efd4550e9c7b
Yevgen Safronov <yevgen@cloudflare.com>
2021-03-18 11:29:40 +0000

    add pagination purging with limit up to 3 pages

--- src/WordPress/Hooks.php
+++ src/WordPress/Hooks.php
@@ -256,6 +256,20 @@ class Hooks
             array_push($listofurls, $pageLink);
         }
 
+        // Refresh pagination
+        $total_posts_count = wp_count_posts()->publish;
+        $posts_per_page = get_option('posts_per_page');
+        // Limit to up to 3 pages
+        $page_number_max = min(3, ceil($total_posts_count / $posts_per_page));
+
+        $this->logger->debug("total_posts_count $total_posts_count");
+        $this->logger->debug("posts_per_page  $posts_per_page");
+        $this->logger->debug("page_number_max $page_number_max");
+
+        foreach (range(1, $page_number_max) as $page_number) {
+            array_push($listofurls, home_url(sprintf('/page/%s/', $page_number)));
+        }
+
         // Attachments
         if ('attachment' == $postType) {
             $attachmentUrls = array();
