b6e81b9ba1bd64d3a12ea31c363d119cb3d44bb4
Léo Colombaro <git@colombaro.fr>
2024-04-02 22:14:38 +0200

    Ensure proper function signatures depending on `psr/log` version

--- src/Integration/DefaultLogger.php
+++ src/Integration/DefaultLogger.php
@@ -6,6 +6,9 @@ use Psr\Log\AbstractLogger;
 use Psr\Log\LogLevel;
 use Psr\Log\LoggerInterface;
 
+# if (Psr\Log version < 2)
+if (class_exists('Psr\Log\Test\DummyTest')):
+
 class DefaultLogger extends AbstractLogger implements LoggerInterface
 {
     private $debug;
@@ -46,3 +49,48 @@ class DefaultLogger extends AbstractLogger implements LoggerInterface
         }
     }
 }
+
+else:
+
+class DefaultLogger extends AbstractLogger implements LoggerInterface
+{
+    private $debug;
+
+    const PREFIX = '[Cloudflare]';
+
+    /**
+     * @param bool|false $debug
+     */
+    public function __construct($debug = false)
+    {
+        $this->debug = $debug;
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param Stringable|string $message
+     * @param array  $context
+     */
+    public function log($level, Stringable|string $message, array $context = []): void
+    {
+        error_log(self::PREFIX.' '.strtoupper($level).': '.$message.' '.
+            (!empty($context) ? print_r($context, true) : ''));
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param Stringable|string $message
+     * @param array  $context
+     */
+    public function debug(Stringable|string $message, array $context = []): void
+    {
+        if ($this->debug) {
+            $this->log(LogLevel::DEBUG, $message, $context);
+        }
+    }
+}
+
+endif;
