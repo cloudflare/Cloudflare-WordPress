0ee4612f4102bd641765c8ec5ae4aecd558e23b2
Barry Kooij <barrykooij@users.noreply.github.com>
2025-12-20 00:08:32 +0100

    Fixed: Set correct type for $logger parameter in deprecated CF\WordPress\Datastore constructor

--- deprecated/WordPress/DataStore.php
+++ deprecated/WordPress/DataStore.php
@@ -5,7 +5,7 @@ namespace CF\WordPress;
  * @deprecated 4.13.0 Use \Cloudflare\APO\WordPress\DataStore instead.
  */
 class DataStore extends \Cloudflare\APO\WordPress\DataStore {
-    public function __construct(\Psr\Log\LoggerInterface $logger) {
+    public function __construct(\CF\Integration\DefaultLogger $logger) {
         _deprecated_file( __FILE__, '4.13.0', "Cloudflare\APO\WordPress\DataStore", 'CF\WordPress namespace is deprecated, use Cloudflare\APO\WordPress instead.' );
         parent::__construct($logger);
     }
