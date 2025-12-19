ac7d9ec7dbdce7d2d52168789277efd364ebfc44
Barry Kooij <barrykooij@users.noreply.github.com>
2025-12-20 00:09:17 +0100

    Changed: Removed contructor parameter types from deprecated CF\Integration\DefaultIntegration to prevent outdated integration errors.

--- deprecated/Integration/DefaultIntegration.php
+++ deprecated/Integration/DefaultIntegration.php
@@ -6,10 +6,10 @@ namespace CF\Integration;
  */
 class DefaultIntegration extends \Cloudflare\APO\Integration\DefaultIntegration {
     public function __construct(
-        \Cloudflare\APO\Integration\ConfigInterface $config,
-        \Cloudflare\APO\Integration\IntegrationAPIInterface $integrationAPI,
-        \Cloudflare\APO\Integration\DataStoreInterface $dataStore,
-        \Psr\Log\LoggerInterface $logger
+        $config,
+        $integrationAPI,
+        $dataStore,
+        $logger
     ) {
         _deprecated_file( __FILE__, '4.13.0', "Cloudflare\APO\Integration\DefaultIntegration", 'CF\Integration namespace is deprecated, use Cloudflare\APO\Integration instead.' );
         parent::__construct($config, $integrationAPI, $dataStore, $logger);
